5ef469315885271f10401998235fc379fbe1f816
IvanYarochkin <Ivan.Yarochkin97@gmail.com>
2017-11-30 17:42:24 +0300

    Add try cache in a sorting method

--- litetext-logic/src/main/java/com/yarachkin/litetext/action/SortingSentenceAction.java
+++ litetext-logic/src/main/java/com/yarachkin/litetext/action/SortingSentenceAction.java
@@ -2,6 +2,7 @@ package com.yarachkin.litetext.action;
 
 import com.yarachkin.litetext.chain.impl.TextHandler;
 import com.yarachkin.litetext.composite.LiteTextComponent;
+import com.yarachkin.litetext.exception.ActionLiteTextException;
 import com.yarachkin.litetext.exception.ChainLiteTextException;
 import com.yarachkin.litetext.exception.CompositeLiteTextException;
 
@@ -10,15 +11,19 @@ import java.util.TreeMap;
 public class SortingSentenceAction {
     private static TreeMap<Integer, LiteTextComponent> sortedSentences;
 
-    public static TreeMap<Integer, LiteTextComponent> printSentencesInAscendingOrder(String text) throws ChainLiteTextException, CompositeLiteTextException {
-        sortedSentences = new TreeMap<>();
-        TextHandler textHandler = new TextHandler();
-        LiteTextComponent textComponent = textHandler.parse(text);
-        for (LiteTextComponent paragraph : textComponent.getComponents()) {
-            calculateSizeSentences(paragraph);
+    public static TreeMap<Integer, LiteTextComponent> printSentencesInAscendingOrder(String text) throws ActionLiteTextException {
+        try{
+            sortedSentences = new TreeMap<>();
+            TextHandler textHandler = new TextHandler();
+            LiteTextComponent textComponent = textHandler.parse(text);
+            for (LiteTextComponent paragraph : textComponent.getComponents()) {
+                calculateSizeSentences(paragraph);
+            }
+            sortedSentences.entrySet().forEach(element -> System.out.println(element.getKey() + " lexemes :" + element.getValue()));
+            return sortedSentences;
+        } catch (ChainLiteTextException | CompositeLiteTextException e) {
+            throw new ActionLiteTextException(e);
         }
-        sortedSentences.entrySet().forEach(element -> System.out.println(element.getKey() + " lexemes :" + element.getValue()));
-        return sortedSentences;
     }
 
     private static void calculateSizeSentences(LiteTextComponent paragraph) throws CompositeLiteTextException {
